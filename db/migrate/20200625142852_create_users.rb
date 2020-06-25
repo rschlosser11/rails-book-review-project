b651dc5f2497c6a832ff785b6094dfdfc199c926
rschlosser11 <rosalinschlosser@gmail.com>
2020-06-25 14:23:55 -0500

    add default value to users table reviewer column

--- db/migrate/20200625142852_create_users.rb
+++ db/migrate/20200625142852_create_users.rb
@@ -4,7 +4,7 @@ class CreateUsers < ActiveRecord::Migration[6.0]
       t.string :name
       t.string :email
       t.string :password_digest
-      t.boolean :reviewer
+      t.boolean :reviewer, default: false
       t.text :bio
 
       t.timestamps
