d174020c5d27b78e70836bd725affeef5424989a
rschlosser11 <rosalinschlosser@gmail.com>
2020-06-29 08:57:22 -0500

    add model relationships and secure password

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,6 @@
 class Review < ApplicationRecord
+  belongs_to :book
+  has_many :comments
+  has_many :users, through: :comments
+  belongs_to :user, -> { where(admin: true) }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  has_many :books, through: :reviews
+  has_many :comments
+  has_many :reviews, through: :comments
+  has_secure_password
+
+  def reviews_written
+    Review.where(user: self)
+  end
 end
