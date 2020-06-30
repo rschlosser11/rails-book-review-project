3a5c22edf1821a390d2ddec91fbc47d1c8e25c7b
rschlosser11 <rosalinschlosser@gmail.com>
2020-06-30 15:51:38 -0500

    add destroy method to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
  def create
    @user = User.new(user_params)
    if @user.save
-     session[:id] = @user.id
+     session[:user_id] = @user.id
      redirect_to user_path(@user)
    else
      render :new
@@ -15,6 +15,11 @@ class UsersController < ApplicationController
 
  def show
    @user = User.find(params[:id])
+ end
+
+ def destroy
+   session.delete(:user_id)
+   redirect_to root_path
  end
 
  private
