b57c7bdbb85b95acd42734e5ead4972e1aa0f774
rschlosser11 <rosalinschlosser@gmail.com>
2020-06-30 16:37:28 -0500

    add book routes, add new method to books controller, add form partial, and new view for books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,3 +1,11 @@
 class BooksController < ApplicationController
+  def new
+    @book = Book.new
+    @author = @book.build_author
+    @genre = @book.build_genre
+  end
 
+  def create
+    raise.params
+  end
 end
