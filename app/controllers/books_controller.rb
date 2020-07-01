a87a8645ee1fb9e13c8ef4358c2609282554b067
rschlosser11 <rosalinschlosser@gmail.com>
2020-07-01 17:20:37 -0500

    add create method for books, update book form partial to have errors, create book show file, update site css for error fields

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,6 +6,21 @@ class BooksController < ApplicationController
   end
 
   def create
-    raise.params
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :summary, :publish_date, :author_id, :genre_id, genre_attributes: [:name], author_attributes: [:name, :bio])
   end
 end
