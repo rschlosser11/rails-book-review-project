2569964c2d196caaf56e45f2e993b0a1ca09ce52
rschlosser11 <rosalinschlosser@gmail.com>
2020-07-02 11:55:52 -0500

    add create method to sessions, update sesssion's new view form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,14 @@ class SessionsController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.find_by(email: params[:email]) if params[:email]
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      redirect_to '/login'
+    end
+  end
 end
