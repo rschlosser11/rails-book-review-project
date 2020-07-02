4a9d4807ae67347fbe8eb457ebf1992daaba8ac0
rschlosser11 <rosalinschlosser@gmail.com>
2020-07-02 11:31:21 -0500

    add login and logout routes, add sessions controller and new method, add sessions folder and new view

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   resources :books
   resources :authors, only: [:index, :show]
   resources :reviews
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 end
