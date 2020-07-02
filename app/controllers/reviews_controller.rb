78a3091f521a9b0c72cc6f7a69c0405056dffbe5
rschlosser11 <rosalinschlosser@gmail.com>
2020-07-02 16:13:05 -0500

    add show and create methods to reviews controller, fix issue with belongs_to in review model, update reviews form partial and outline show view

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,4 +4,25 @@ class ReviewsController < ApplicationController
     @book_id = params[:book_id] if params[:book_id]
     @books = Book.all
   end
+
+  def create
+    @review = Review.new(review_params)
+    @books = Book.all
+    @book_id = params[:review][:book_id] if params[:review][:book_id]
+    if @review.save
+      redirect_to review_path(@review)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @review = Review.find(params[:id])
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:book_id, :title, :blurb, :content, :user_id, :rating)
+  end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,10 +2,10 @@ class Review < ApplicationRecord
   belongs_to :book
   has_many :comments
   has_many :users, through: :comments
-  belongs_to :user, -> { where(admin: true) }
+  belongs_to :user, -> { where(reviewer: true) }
   validates :title, :content, :blurb, :book, presence: true
   validates :rating, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
-  validates :blurb, presence: true, length: { maximum: 280 }
+  validates :blurb, presence: true, length: { maximum: 500 }
   validates :book, uniqueness: { scope: :user, message: "already wrote a review for this book"}
   accepts_nested_attributes_for :book
 end
