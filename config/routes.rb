cbb81f52da3b2ca6e4377a23ff21a2012c860409
rschlosser11 <rosalinschlosser@gmail.com>
2020-07-07 13:57:34 -0500

    add .env file and callback route

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,6 @@ Rails.application.routes.draw do
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
+
+  get '/auth/facebook/callback', to: "sessions#create"
 end
