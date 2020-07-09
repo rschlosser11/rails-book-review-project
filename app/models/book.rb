bf93881ba21773c5e258bc36a591c1c5122fcd1a
rschlosser11 <rosalinschlosser@gmail.com>
2020-07-09 12:13:53 -0500

    add validations in book model for cover and title uniqueness

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,12 +2,18 @@ class Book < ApplicationRecord
   belongs_to :author
   belongs_to :genre
   has_many :reviews
-  validates :title, :summary, :publish_year, presence: true
+  has_one_attached :cover
+  validates :title, :summary, :publish_year, :cover, presence: true
+  validates :title, uniqueness: { scope: :author, case_sensitive: false, message: "already exists for this author" }
   accepts_nested_attributes_for :genre, reject_if: :all_blank
   accepts_nested_attributes_for :author, reject_if: :all_blank
-  has_one_attached :cover
+
 
   def self.title_start_with(letter)
       where("title LIKE ?", "#{letter}%")
+  end
+
+  def same_author
+    Book.find_by(title: self.title)
   end
 end
